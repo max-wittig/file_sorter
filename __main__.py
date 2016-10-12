9dacb6e774085eda877770bdf1d138b39d63e429
Max Wittig <max.wittig95@gmail.com>
2016-10-12 23:59:19 +0200

    bugfixes

--- __main__.py
+++ __main__.py
@@ -6,14 +6,13 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser("file_sorter")
-    parser.add_argument("-d", "--directory", help="Which directory to sort")
+    parser.add_argument("-d", "--directory", help="Which directory to sort", required=True)
     return vars(parser.parse_args())
 
 
 def main():
     options = get_args()
     location = options["directory"]
-    print(location)
     file_sorter = FileSorter(location)
     file_sorter.sort_files()
     #file_sorter.file_helper.generate_random_files()
